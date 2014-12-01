f10506bfffcfba3b0d54c7afb57fe60c4ba1348e
arrack <arrack@pixnet.tw>
2014-12-01 11:27:35 +0800

    add createbranch.sh

---
+++ bin/createbranch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+TNUM="$1"
+if [ "$TNUM" = "" ]; then
+  read -p "Enter ticket number" TNUM
+else
+fi
+echo "ticket number: $TNUM"
+#git branch "ticket$TNUM"
+#git push origin "ticket$TNUM"
+#git branch -d "ticket$TNUM"
+#git checkout "ticket$TNUM"
+#
+
+git checkout -b "ticket$TNUM"
+git push --set-upstream origin "ticket$TNUM"
