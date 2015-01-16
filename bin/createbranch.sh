366b524a606dcc69e3b6b37425c5199323d56ed3
arrack <arrack@pixnet.tw>
2015-01-16 09:38:03 +0800

    remove unuse code

--- bin/createbranch.sh
+++ bin/createbranch.sh
@@ -5,11 +5,6 @@ if [ "$TNUM" = "" ]; then
 else
 fi
 echo "ticket number: $TNUM"
-#git branch "ticket$TNUM"
-#git push origin "ticket$TNUM"
-#git branch -d "ticket$TNUM"
-#git checkout "ticket$TNUM"
-#
 git pull --rebase -v -p
 git checkout -b "ticket$TNUM"
 git push --set-upstream origin "ticket$TNUM"
